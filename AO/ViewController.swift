aece8ab70b5cae44fc9424cc05585c0ca40ca430
summerlillie <110996638+summerlillie@users.noreply.github.com>
2022-08-10 14:42:16 -0400

    Add files via upload

---
+++ AO/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  AO
+//
+//  Created by Scholar on 8/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var appTitle: UILabel!
+    
+    @IBOutlet weak var textField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func submitButtonTapped(_ sender: Any) {
+        if let newTitle = textField.text {
+            appTitle.text = newTitle
+        }
+        
+    }
+}
+
